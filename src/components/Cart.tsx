138e0b39b1e25afbae0c3a1a16518255a24f104e
Federico Galassi - Front End Dev <94568896+fedega15@users.noreply.github.com>
2023-11-12 16:24:02 -0300

    ASD

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,33 +5,31 @@ import CartLineItem from "./CartLineItem"
 const Cart = () => {
     const [confirm, setConfirm] = useState<boolean>(false)
     const { dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart } = useCart()
+    
     const onSubmitOrder = () => {
         dispatch({ type: REDUCER_ACTIONS.SUBMIT })
         setConfirm(true)
     }
     const pageContent = confirm
-        ? <h2>¡GRACIAS POR COMPRAR! <p>Un asesor te contactara a continuacion!</p></h2>
+        ? <h2>¡GRACIAS POR COMPRAR! Un asesor te contactara a continuacion!</h2>
         
         : <>
         
             <ul  className="cart">
-                 {cart.map(item => (
-                    
-                    <div key={item.sku}>
+            {cart.map(item => {
+                    return (
                         <CartLineItem
+                            key={item.sku}
                             item={item}
                             dispatch={dispatch}
                             REDUCER_ACTIONS={REDUCER_ACTIONS}
                         />
-                        
-                        <hr />
-                    </div>
-                ))}
-                
+                    )
+                })}
             </ul>
             <div className="cart__totals">
                 <button className="cart__submit" disabled={!totalItems} onClick={onSubmitOrder}>
-                    Comprar {totalPrice}
+                    Comprar{/*  {totalPrice} */}
                 </button>
             </div>
         </>
